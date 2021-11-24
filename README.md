622c4763cc70e236368b5cbdf7895629ffeaa3f1
Dionatam <djouvh@gmail.com>
2021-11-23 22:34:13 -0300

    Conexão com o banco de dados no arquivo app.js

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sistema-de-Biblioteca
+
+Iniciando o projeto
+
+---Dependencias---
+
+*npm init -y
+Inicia o package.json
+
+*npm install mongodb
+Classificado como um programa de banco de dados NoSQL, o MongoDB usa documentos semelhantes a JSON com esquemas, os dados podem ser aninhados em hierarquias complexas e continuar a ser indexáveis e fáceis de buscar. O banco de dados não relacional da aplicação.
+
+*npm install mongoose
+Mongoose é uma ferramenta de modelagem de objetos MongoDB projetada para funcionar em um ambiente assíncrono. O Mongoose oferece suporte a promessas e retornos de chamada.
+
+*npm install ejs
+Chama-se Embedded JavaScript templating (EJS) e é uma linguagem de modelagem simples que permite gerar marcação HTML com JavaScript simples.
+
+*npm install express
+Inicia a biblioteca express para as requisições HTTP no node, criou junto a pasta node_modules e o package-lock.json
+
+*Criei o arquivo .gitignore
+Indicando o node_modules/ evita o envio da pasta node_modules para o github
+
+*npm install --prod
+Resgatando o node_modules
---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+const port = 3000
+
+
+mongoose.connect('mongodb+srv://biblioteca:12345@cluster0.dg8eg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+
+// CRIANDO UM MODELO  PARA INCLUSÃO DE LIVROS NO BANCO DE DADOS
+const livros = mongoose.model('Cadastro de Livros', {
+  titulo: String,
+  nomeAutor: String,
+  anoPublicacao: Number,
+})
+
+// CRIANDO UM MODELO  PARA INCLUSÃO DE ALUNOS NO BANCO DE DADOS
+const alunos = mongoose.model('Cadastro de Alunos', {
+  nome: String,
+  email: String,
+  senha: String,
+})
+
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.use(express.static(__dirname + '/public'))
+
+// ROTAS
+
+// criei essa rota so pra testar, coloquei um h1 na pagina atualizacaoaluno.ejs, como teste.
+app.get('/', (req, res) => {
+  res.render('atualizacaoaluno')
+})
+
+
+// Ouvinte da porta de acesso
+app.listen(port, () => {
+  console.log('Biblioteca no ar, na porta: ' + port)
+})
